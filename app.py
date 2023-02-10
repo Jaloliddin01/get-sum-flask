813a3812c5f92bc494bc49e5ad30f7d83c7daa6d
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-02-10 17:07:19 +0500

    sum api

--- app.py
+++ app.py
@@ -5,23 +5,9 @@ app = Flask(__name__)
 
 @app.route('/api/get-sum/', methods=['GET'])
 def get_sum():
-    """
-    this function returns sum of two number from request data.
-
-    Returns:
-        json: sum of two number
-
-    Note:
-        request data will be like this:
-            /api/get-sum/?a=1&b=2
-            
-        response data will be like this:
-            {
-                "sum": 3
-            }
-    """
-    pass
+    
+    return { "sum" : sum(map(int, request.args.values())) }
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
